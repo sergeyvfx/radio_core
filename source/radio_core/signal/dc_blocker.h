9f52d3eb764e642bb5f474c0889ddded3b979821
Sergey Sharybin <sergey.vfx@gmail.com>
2025-01-04 16:31:30 +0100

    Allow processing multiple samples by DCBlocker
    There is no explicit advantage to it from the performance point of
    view, it just simplifies writing processing code that handle an entire
    buffer of samples.

--- source/radio_core/signal/dc_blocker.h
+++ source/radio_core/signal/dc_blocker.h
@@ -15,6 +15,9 @@
 
 #pragma once
 
+#include <cassert>
+#include <span>
+
 namespace radio_core::signal {
 
 template <class SampleType, class RealType = SampleType>
@@ -27,6 +30,32 @@ class DCBlocker {
     xml_ = x;
     yml_ = y;
     return y;
+  }
+
+  // Apply DC block on multiple input samples, writing the result to the
+  // corresponding element of the output buffer:
+  //
+  //   output_samples = [ dc_block(x) for x in input_samples ]
+  //
+  // The input and output buffers are allowed to be the same.
+  // Aliasing of the input and output buffers might lead to an undefined
+  // behavior.
+  //
+  // Returns subspan of output where samples were actually written.
+  //
+  // The caller must ensure the output samples buffer is big enough should have
+  // at least the size of the input samples).
+  inline auto operator()(const std::span<const SampleType> input_samples,
+                         const std::span<SampleType> output_samples)
+      -> std::span<SampleType> {
+    assert(input_samples.size() <= output_samples.size());
+
+    const size_t num_input_samples = input_samples.size();
+    for (size_t i = 0; i < num_input_samples; ++i) {
+      output_samples[i] = (*this)(input_samples[i]);
+    }
+
+    return output_samples.subspan(0, num_input_samples);
   }
 
  private:
--- source/radio_core/signal/internal/dc_blocker_test.cc
+++ source/radio_core/signal/internal/dc_blocker_test.cc
@@ -4,25 +4,40 @@
 
 #include "radio_core/signal/dc_blocker.h"
 
+#include <vector>
+
 #include "radio_core/math/complex.h"
+#include "radio_core/unittest/mock.h"
 #include "radio_core/unittest/test.h"
 
 namespace radio_core::signal {
 
+using testing::Each;
+using testing::FloatNear;
+
 TEST(DCBlocker, Basic) {
   DCBlocker<float> dc_blocker(0.8f);
 
-  int phi = 0;
-
   // Push a lot of samples to stabilize the blocker.
-  for (int a = 0; a < 100; ++a, ++phi) {
+  for (int a = 0; a < 100; ++a) {
     dc_blocker(1.21f);
   }
 
   // Ensure constant offset is removed.
-  for (int a = 0; a < 10; ++a, ++phi) {
+  for (int a = 0; a < 10; ++a) {
     EXPECT_NEAR(dc_blocker(1.21f), 0.0f, 1e-6f);
   }
+}
+
+TEST(DCBlocker, BasicMultipleSampels) {
+  const std::vector<float> input_samples(128, 1.21f);
+  std::vector<float> output_samples(128);
+
+  DCBlocker<float> dc_blocker(0.8f);
+  dc_blocker(input_samples, output_samples);
+  dc_blocker(input_samples, output_samples);
+
+  EXPECT_THAT(output_samples, Each(FloatNear(0.0f, 1e-6f)));
 }
 
 TEST(DCBlocker, Complex) {
@@ -32,15 +47,13 @@ TEST(DCBlocker, Complex) {
 
   DCBlocker<Complex, float> dc_blocker(0.8f);
 
-  int phi = 0;
-
   // Push a lot of samples to stabilize the blocker.
-  for (int a = 0; a < 100; ++a, ++phi) {
+  for (int a = 0; a < 100; ++a) {
     dc_blocker(Complex(1.21f));
   }
 
   // Ensure constant offset is removed.
-  for (int a = 0; a < 10; ++a, ++phi) {
+  for (int a = 0; a < 10; ++a) {
     EXPECT_NEAR(Norm(dc_blocker(Complex(1.21f))), 0.0f, 1e-6f);
   }
 }
