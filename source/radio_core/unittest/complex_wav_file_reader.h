096a461f223c687a35b2ad9c5cc90b43db84d6db
Sergey Sharybin <sergey.vfx@gmail.com>
2025-01-15 00:48:56 +0100

    Expand functionality of ComplexWAVFileReader
    - Allow reading format specification from a file without retaining
      any state.
    
    - Implement buffered reading: the callback is invoked for a buffer
      of complex samples read from the file.

--- source/radio_core/unittest/complex_wav_file_reader.h
+++ source/radio_core/unittest/complex_wav_file_reader.h
@@ -8,8 +8,10 @@
 
 #pragma once
 
+#include <array>
 #include <filesystem>
 #include <functional>
+#include <optional>
 
 #include "radio_core/math/complex.h"
 #include "radio_core/unittest/test.h"
@@ -67,10 +69,11 @@ class ComplexWAVFileReader {
 
   // Invoke given callback with the given arguments and a complex sample from
   // the file.
+  // Returns true if all samples from the file have been read.
   template <class F, class... Args>
   auto ForeachSample(F&& callback, Args&&... args) -> bool {
     bool all_samples_ok = true;
-    bool read_result = wav_reader_.ReadAllSamples<float, 2>(
+    const bool read_result = wav_reader_.ReadAllSamples<float, 2>(
         [&](const std::span<float> sample) {
           if (sample.size() != 2) {
             all_samples_ok = false;
@@ -81,6 +84,66 @@ class ComplexWAVFileReader {
               std::forward<F>(callback), std::forward<Args>(args)..., x);
         });
     return read_result && all_samples_ok;
+  }
+
+  // Invoke given callback with the given arguments and a buffer of complex
+  // samples read from the file. Returns true if all samples from the file have
+  // been read.
+  template <size_t BufferSize, class F, class... Args>
+  auto ForeachSampleBuffered(F&& callback, Args&&... args) -> bool {
+    std::array<Complex, BufferSize> buffer;
+
+    bool all_samples_ok = true;
+
+    // The number of samples in the buffer.
+    size_t num_samples = 0;
+
+    wav_reader_.template ReadAllSamples<float, 2>(
+        [&](const std::span<const float> frame_samples) {
+          if (frame_samples.size() != 2) {
+            all_samples_ok = false;
+            return;
+          }
+
+          // Convert the WAV file frame samples to the buffer sample type.
+          buffer[num_samples++] = Complex(frame_samples[0], frame_samples[1]);
+
+          // Invoke the callback with the full buffer if it is full.
+          if (num_samples == BufferSize) {
+            std::invoke(
+                std::forward<F>(callback), std::forward<Args>(args)..., buffer);
+            num_samples = 0;
+          }
+        });
+
+    // Invoke the callback with the trailing samples of a partially full buffer
+    // at the end of the file.
+    if (num_samples != 0) {
+      std::invoke(std::forward<F>(callback),
+                  std::forward<Args>(args)...,
+                  std::span(buffer.data(), num_samples));
+    }
+
+    return all_samples_ok;
+  }
+
+  // Read format specification from the file without retaining any state.
+  // Returns std::nullopt if fle read fails.
+  static auto ReadFormatSpecFromFile(const Path& path)
+      -> std::optional<FormatSpec> {
+    ComplexWAVFileReader file;
+    if (!file.Open(path)) {
+      return std::nullopt;
+    }
+
+    return file.GetFormatSpec();
+  }
+
+  // Read format specification from a while within the test data storage.
+  // Returns std::nullopt if fle read fails.
+  static auto ReadFormatSpecFromTestDataFile(const Path& path)
+      -> std::optional<FormatSpec> {
+    return ReadFormatSpecFromFile(TestFileAbsolutePath(path));
   }
 
  private:
--- source/radio_core/unittest/internal/complex_wav_file_reader_test.cc
+++ source/radio_core/unittest/internal/complex_wav_file_reader_test.cc
@@ -5,26 +5,54 @@
 #include "radio_core/unittest/complex_wav_file_reader.h"
 
 #include <functional>
+#include <vector>
 
 #include "radio_core/unittest/test.h"
 
 namespace radio_core::testing {
 
-TEST(ComplexWAVFileReader, Basic) {
-  using Path = std::filesystem::path;
+using Path = std::filesystem::path;
 
-  ComplexWAVFileReader reader;
-
+TEST(ComplexWAVFileReader, GetFormatSpec) {
   const Path filename = testing::TestFileAbsolutePath(
       Path("modulation") / "bpsk" / "bpsk_pn23_665.4ksps_offset.wav");
 
+  ComplexWAVFileReader reader;
   EXPECT_TRUE(reader.Open(filename));
 
   const ComplexWAVFileReader::FormatSpec format_spec = reader.GetFormatSpec();
   EXPECT_EQ(format_spec.num_channels, 2);
+}
 
-  EXPECT_TRUE(
-      reader.ForeachSample([](const Complex& sample) { (void)sample; }));
+TEST(ComplexWAVFileReader, Basic) {
+  const Path filename = testing::TestFileAbsolutePath(
+      Path("modulation") / "bpsk" / "bpsk_pn23_665.4ksps_offset.wav");
+
+  ComplexWAVFileReader reader;
+  EXPECT_TRUE(reader.Open(filename));
+
+  std::vector<Complex> all_samples;
+  EXPECT_TRUE(reader.ForeachSample(
+      [&](const Complex& sample) { all_samples.push_back(sample); }));
+  EXPECT_EQ(all_samples.size(), 65536 * 4);
+}
+
+TEST(ComplexWAVFileReader, Buferred) {
+  const Path filename = testing::TestFileAbsolutePath(
+      Path("modulation") / "bpsk" / "bpsk_pn23_665.4ksps_offset.wav");
+
+  ComplexWAVFileReader reader;
+  EXPECT_TRUE(reader.Open(filename));
+
+  std::vector<Complex> all_samples;
+  // Read in the buffer of size that is not an integer multiple of the overall
+  // number of samples.
+  EXPECT_TRUE(reader.ForeachSampleBuffered<101>(
+      [&](const std::span<const Complex> new_samples) {
+        all_samples.insert(
+            all_samples.end(), new_samples.begin(), new_samples.end());
+      }));
+  EXPECT_EQ(all_samples.size(), 65536 * 4);
 }
 
 }  // namespace radio_core::testing
