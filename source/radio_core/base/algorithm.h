a9d7043f788ece9539658f62c299783a42327578
Sergey Sharybin <sergey.vfx@gmail.com>
2025-01-26 12:55:38 +0100

    Expand documentation of CircularShift
    Makes it easier to transfer algorithms between different languages
    for tests and prototypes.

--- source/radio_core/base/algorithm.h
+++ source/radio_core/base/algorithm.h
@@ -27,7 +27,23 @@ constexpr auto Max(const T& a, const T& b) -> const T& {
 // This function can be used for any C++ style container, such as std::span.
 //
 // This function behaves similar to the numpy.roll() and Matlab's circshift()
-// with the difference that here it essentially operates on a single axis.
+// with the difference that here it essentially operates on a single axis. This
+// includes matching in the direction of shift based on the sign of the shift
+// argument.
+//
+// For a single axis input array:
+//
+// - CircularShift(array, shift) is a direct equivalent to numpy.roll(array,
+//   shift).
+//
+//   Example: numpy.roll([0, 1, 2, 3, 4, 5], 13) produces the same output as
+//            CircularShift({0, 1, 2, 3, 4, 5}, 13): [5, 0, 1, 2, 3, 4].
+//
+// - CircularShift(array, shift) is a direct equivalent to Matlab's
+//   circshift(array, shift).
+//
+//   Example: circshift([0, 1, 2, 3, 4, 5], 13) produces the same output as
+//            CircularShift({0, 1, 2, 3, 4, 5}, 13): [5, 0, 1, 2, 3, 4].
 template <class Container>
 constexpr auto CircularShift(Container& container, const int shift) {
   if (shift == 0) {
